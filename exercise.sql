4ec6cc36f5300cd44cac7813568e81716cd443f6
PickleRick1488 <2104485@talnet.nl>
2020-02-02 03:35:58 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,16 @@
+use sterrenstelsel;
+alter table planeten 
+	add Zon varchar(40)  not null;
+    
+alter table planeten 
+	add Mercurius varchar(40)  not null;
+    
+alter table planeten 
+	add Venus varchar(40)  not null;
+    
+alter table planeten 
+	add Aarde varchar(40)  not null;
+    
+alter table planeten 
+	add Mars varchar(40)  not null;
+    
